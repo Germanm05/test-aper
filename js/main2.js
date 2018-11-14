3131b810b5bcdf73bb342e3a8317c4021e725c22
German <gmuriel05@gmail.com>
2018-11-14 10:56:33 -0300

    cambios

--- js/main2.js
+++ js/main2.js
@@ -3,14 +3,14 @@ $(document).ready(function () {
     // Get a reference to the 'Send Message' button.
     var btn = document.getElementById('btn-send');
 
-    var ORIGIN = "http://aper.test:1084";
+    var ORIGIN = 'file:';
 
     function sendMessage(e) {
         // Prevent any default browser behaviour.
         e.preventDefault();
 
         // Send a message with the text 'Hello Treehouse!' to the receiver window.
-        window.parent.postMessage(JSON.stringify({ 'newMessage': 'Mensaje desde Ventana!' }), ORIGIN);
+        window.parent.postMessage(JSON.stringify({ 'newMessage': 'Mensaje desde Ventana!' }), (ORIGIN == 'file:' ? '*' : ORIGIN));
         window.close()
     }
 
