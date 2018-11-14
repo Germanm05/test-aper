dd5d80cdeccb06dbd18cf442c94363d20a234059
German <gmuriel05@gmail.com>
2018-11-14 10:21:04 -0300

    cambios

--- js/main2.js
+++ js/main2.js
@@ -3,31 +3,20 @@ $(document).ready(function () {
     // Get a reference to the 'Send Message' button.
     var btn = document.getElementById('btn-send');
 
-
-    window.addEventListener('message', function(e) {
-        if (event.origin !== "http://aper.test:1084")
-            return;
-        var message = e.data;
-        console.log(message);
-        $('#receiver').text(message);
-        addEventListener('click', sendMessage);
-
-    }, false);
-
+    var ORIGIN = "http://aper.test:1084";
 
     function sendMessage(e) {
         // Prevent any default browser behaviour.
         e.preventDefault();
 
         // Send a message with the text 'Hello Treehouse!' to the receiver window.
-        window.opener.postMessage('Mensaje desde Ventana!', 'http://aper.test:1084');
+        window.opener.postMessage(JSON.stringify({ 'newMessage': 'Mensaje desde Ventana!' }), ORIGIN);
         window.close()
     }
 
     // Add an event listener that will execute the sendMessage() function
     // when the send button is clicked.
     btn.addEventListener('click', sendMessage);
-
 
 });
 
